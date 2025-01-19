5f3ab3128d3221de2554ca98327ca4911fce8e3d
AidanLam06 <aidanlam02@gmail.com>
2025-01-19 03:40:56 +0000

    Add circuit jpeg and main code file

---
+++ PEISproject.py
@@ -0,0 +1,56 @@
+from RPi import GPIO as GPIO
+import time
+import os
+from twilio.rest import Client
+
+input_pin = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+account_sid = 'AC3dc9d2fe169c0346833576e2d39de09c'
+
+token = 'aabad8eda0431c484de3b76240d50de2' #hide this one
+
+client = Client(account_sid, token)
+
+
+previous_state = None
+
+def check_sensor():
+    global previous_state
+    try:
+        sensor_state = GPIO.input(input_pin)
+        
+        if sensor_state == GPIO.HIGH:
+            print('Sensor is NOT blocked (GPIO HIGH)')
+            if previous_state == GPIO.LOW: 
+                print("State changed: unblocked.")
+                time.sleep(2)
+        else:
+            print('Sensor is blocked (GPIO LOW)')
+            if previous_state != GPIO.LOW: 
+                print("State changed: blocked. Sending notification...")
+                message = client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    to='whatsapp:+18197022276',
+                    body='Motion Detected'
+                )
+                print(f"Alert dispatched: {message.sid}")
+                time.sleep(2)
+        
+        previous_state = sensor_state
+
+    except Exception as e:
+        print(f"Error: {e}")
+        GPIO.cleanup()
+
+# Main loop
+try:
+    print
+    while True:
+        check_sensor()
+        time.sleep(0.1) 
+except KeyboardInterrupt:
+    print("Exiting program...")
+    GPIO.cleanup()
